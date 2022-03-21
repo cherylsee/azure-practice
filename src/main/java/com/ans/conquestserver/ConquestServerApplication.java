1bdbcbb1429c6d131cbe1ce957930da1beb3024d
Cheryl See <cheryl.see@revature.net>
2022-03-21 12:37:13 -0500

    First commit

---
+++ src/main/java/com/ans/conquestserver/ConquestServerApplication.java
@@ -0,0 +1,13 @@
+package com.ans.conquestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConquestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConquestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ans/conquestserver/ConquestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ans.conquestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConquestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
